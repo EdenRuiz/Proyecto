03506f82bd2508db6c8140275614cdb0ad0ad1be
EdenRuiz <eden_olea@hotmail.com>
2025-01-13 10:33:52 -0800

    Update Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Proteínas Saludables. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
